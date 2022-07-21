d026a6a9ee3ef29edc7abec95e65695ee83757e7
LChartrand <jl_chartrand@hotmail.com>
2022-07-21 16:55:41 -0400

    fix some comments

--- src/ENLPModels.jl
+++ src/ENLPModels.jl
@@ -7,13 +7,13 @@ using NLPModels
 
 
 include("extendedfuncs.jl")
-#export all functions defined in ENLPModels
+#export all functions defined in ENLPModels.
 for n in names(@__MODULE__; all=true)
     if Base.isidentifier(n) && !(n in (Symbol(@__MODULE__), :eval, :include))
         @eval export $n
     end
 end
-#Export all functions exported by NLPModels
+#Export all functions exported by NLPModels.
 for n in names(NLPModels; all=true)
     if Base.isidentifier(n) && !(n in (Symbol(NLPModels), :eval, :include))
         @eval export $n
